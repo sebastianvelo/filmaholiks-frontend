98060c78936fe48e1715a127701d0d37bc82a92c
Sebastian Velo <sebastian.velo@mercadolibre.com>
2021-10-20 08:08:50 -0300

    Changes in navigation

--- src/client/layout/navigation/header/NavigationHeader.tsx
+++ src/client/layout/navigation/header/NavigationHeader.tsx
@@ -1,20 +1,18 @@
-import { HamburgerSvg } from "client/common/components/svg/Svg";
 import { FunctionComponent } from "react";
+import Brand, { BrandProps } from "./brand/Brand";
 import Menu, { MenuProps } from "./menu/Menu";
+import ActionsToggler, { ActionsTogglerProps } from "./toggler/ActionsToggler";
 
-export interface NavigationHeaderProps {
+export interface NavigationHeaderProps extends ActionsTogglerProps, BrandProps {
   header?: string;
-  toggleLinks?: () => void;
   menu: MenuProps;
 }
 
 const NavigationHeader: FunctionComponent<NavigationHeaderProps> = (props: NavigationHeaderProps) => {
   return (
     <div className={`flex justify-between items-center w-full pt-2 pb-2 lg:pb-0`}>
-      <div className={`block sm:hidden`} onClick={props.toggleLinks!}>
-        <HamburgerSvg />
-      </div>
-      <header className={'text-4xl px-3 font-extrabold tracking-tighter	hover:tracking-widest	transition-letter-spacing duration-200 ease-in'}>{props.header}</header>
+      <ActionsToggler {...props} />
+      <Brand {...props} />
       <Menu {...props.menu} />
     </div>
   );
